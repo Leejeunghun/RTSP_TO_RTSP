94cf7296c2e312396011102df502ddad2531c760
Leejeunghun <201401662@inu.ac.kr>
2022-04-11 17:34:59 +0900

    쓰레드 추가 RTSP 받을 경우 쓰레드에서 처리되도록 기능 추가

--- stream.bash
+++ stream.bash
@@ -1,6 +1,6 @@
 python3 stream.py \
-	--device_id "rtsp://192.168.100.10:554/user=admin&password=&channel=1&stream=0.sdp" \
-	--fps 15 \
+	--device_id "rtsp://192.168.48.10:554/user=admin&password=&channel=1&stream=0.sdp" \
+	--fps 10 \
 	--image_width 640 \
 	--image_height 480 \
 	--stream_uri /video_stream
--- stream.py
+++ stream.py
@@ -10,11 +10,16 @@ import gi
 import cv2
 import argparse
 
+# 쓰레드 추가
+from threading import Thread
+import time
 # import required library like Gstreamer and GstreamerRtspServer
 gi.require_version('Gst', '1.0')
 gi.require_version('GstRtspServer', '1.0')
 from gi.repository import Gst, GstRtspServer, GObject
 
+
+        
 # Sensor Factory class which inherits the GstRtspServer base class and add
 # properties to it.
 class SensorFactory(GstRtspServer.RTSPMediaFactory):
@@ -24,42 +29,42 @@ class SensorFactory(GstRtspServer.RTSPMediaFactory):
         self.number_frames = 0
         self.fps = opt.fps
         self.duration = 1 / self.fps * Gst.SECOND  # duration of a frame in nanoseconds
+        
         self.launch_string = 'appsrc name=source is-live=true block=true format=GST_FORMAT_TIME ' \
                              'caps=video/x-raw,format=BGR,width={},height={},framerate={}/1 ' \
                              '! videoconvert ! video/x-raw,format=I420 ' \
                              '! x264enc speed-preset=ultrafast tune=zerolatency ' \
                              '! rtph264pay config-interval=1 name=pay0 pt=96' \
                              .format(opt.image_width, opt.image_height, self.fps)
+        #쓰레드 추가
+        self.VideoList = []
+        self.thread_monitor = self.MonitorThread()
+        self.thread_monitor.start()
+        
     # method to capture the video feed from the camera and push it to the
     # streaming buffer.
     def on_need_data(self, src, length):
-        if self.cap.isOpened():
-            ret, frame = self.cap.read()
-            if ret:
-                # It is better to change the resolution of the camera 
-                # instead of changing the image shape as it affects the image quality
-                frame = cv2.resize(frame, (opt.image_width, opt.image_height),interpolation = cv2.INTER_LINEAR)
-                
+        if self.thread_monitor.Video_List:
+          # It is better to change the resolution of the camera 
+           # instead of changing the image shape as it affects the image quality
                 # Enter your Sample code 
-
-                frame = cv2.GaussianBlur(frame,(5,5),0) #Sample Code 
-            
-                
-                #######################
-                data = frame.tostring()
-                buf = Gst.Buffer.new_allocate(None, len(data), None)
-                buf.fill(0, data)
-                buf.duration = self.duration
-                timestamp = self.number_frames * self.duration
-                buf.pts = buf.dts = int(timestamp)
-                buf.offset = timestamp
-                self.number_frames += 1
-                retval = src.emit('push-buffer', buf)
-                print('pushed buffer, frame {}, duration {} ns, durations {} s'.format(self.number_frames,
+#                frame = cv2.GaussianBlur(frame,(5,5),0) #Sample Code 
+                ######################
+            frame = self.thread_monitor.Video_List.pop(0)
+            data = frame.tostring()
+            buf = Gst.Buffer.new_allocate(None, len(data), None)
+            buf.fill(0, data)
+            buf.duration = self.duration
+            timestamp = self.number_frames * self.duration
+            buf.pts = buf.dts = int(timestamp)
+            buf.offset = timestamp
+            self.number_frames += 1
+            retval = src.emit('push-buffer', buf)
+            print('pushed buffer, frame {}, duration {} ns, durations {} s'.format(self.number_frames,
                                                                                        self.duration,
                                                                                        self.duration / Gst.SECOND))
-                if retval != Gst.FlowReturn.OK:
-                    print(retval)
+            if retval != Gst.FlowReturn.OK:
+                print(retval)
     # attach the launch string to the override method
     def do_create_element(self, url):
         return Gst.parse_launch(self.launch_string)
@@ -69,6 +74,20 @@ class SensorFactory(GstRtspServer.RTSPMediaFactory):
         self.number_frames = 0
         appsrc = rtsp_media.get_element().get_child_by_name('source')
         appsrc.connect('need-data', self.on_need_data)
+
+    class MonitorThread(Thread):
+        def __init__(self):
+            Thread.__init__(self)
+            self.cap = cv2.VideoCapture(opt.device_id)
+            self.Video_List=[]
+
+        def run(self):
+            while(1):
+                ret, frame = self.cap.read()
+                frame = cv2.resize(frame, (opt.image_width, opt.image_height),interpolation = cv2.INTER_LINEAR)
+                self.Video_List.append(frame)
+                self.duration = 1 / opt.fps  # duration of a frame in nanoseconds
+                #time.sleep(self.duration)
 
 # Rtsp server implementation where we attach the factory sensor with the stream uri
 class GstServer(GstRtspServer.RTSPServer):
@@ -96,7 +115,9 @@ try:
 except ValueError:
     pass
 
-# initializing the threads and running the stream on loop.
+
+
+#initializing the threads and running the stream on loop.
 GObject.threads_init()
 Gst.init(None)
 server = GstServer()

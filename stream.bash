f0b1a67751c382744dd7a2b6119feb735e22f4ac
Leejeunghun <201401662@inu.ac.kr>
2022-05-11 09:51:30 +0900

    ip주소 변경

--- stream.bash
+++ stream.bash
@@ -1,5 +1,5 @@
 python3 stream.py \
-	--device_id "rtsp://192.168.48.10:554/user=admin&password=&channel=1&stream=0.sdp" \
+	--device_id "rtsp://192.168.45.211:554/user=admin&password=&channel=1&stream=0.sdp" \
 	--fps 10 \
 	--image_width 640 \
 	--image_height 480 \
